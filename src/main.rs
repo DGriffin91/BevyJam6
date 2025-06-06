5c3807e28f9455560e7d93e1d278f1f67b91b360
DGriffin91 <github@dgdigital.net>
2025-06-05 18:50:41 -0700

    subsecond

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+/*
+Windows hotpatch note:
+set BEVY_ASSET_ROOT=.
+set CARGO_TARGET_DIR=
+dx serve --hot-patch
+Save file before first run to trigger initial rebuild
+*/
+
 use bevy::asset::AssetMetaCheck;
 use bevy::prelude::*;
+use bevy_simple_subsecond_system::prelude::*;
 
 fn main() {
     App::new()
@@ -10,14 +19,30 @@ fn main() {
             meta_check: AssetMetaCheck::Never,
             ..default()
         }))
+        .add_plugins(SimpleSubsecondPlugin::default())
         .add_systems(Startup, setup)
+        .add_systems(Update, greet)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+#[hot]
+fn greet(time: Res<Time>) {
+    info_once!(
+        "Hello from a hotpatched system! Try changing this string while the app is running! Patched at t = {} s!!",
+        time.elapsed_secs()
+    );
+}
+use bevy::color::palettes::css::RED;
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     commands.spawn(Camera2d);
-    commands.spawn(Sprite {
-        image: asset_server.load("ducky.png"),
-        ..Default::default()
-    });
+    commands.spawn((
+        Mesh2d(meshes.add(Circle::new(100.0))),
+        MeshMaterial2d(materials.add(Color::from(RED))),
+        Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+    ));
 }
